ab99c3ffa0fe2dbdcfde5378d6b324305faa562b
Fernando Morato <fernandomorato93@gmail.com>
2025-06-19 21:30:05 -0300

    add new telescope keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -52,16 +52,21 @@ return {
 			pcall(require("telescope").load_extension, "ui-select")
 
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = "[S]earch Recent Files ('.' for repeat)" })
-			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-		end,
+      local keymap = vim.keymap.set
+      local opts = { silent = true, noremap = true }
+			keymap("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+			keymap("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+			keymap("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			keymap("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+			keymap("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+			keymap("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			keymap("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			keymap("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+			keymap("n", "<leader>s.", builtin.oldfiles, { desc = "[S]earch Recent Files ('.' for repeat)" })
+			keymap("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+      keymap("n", 'gd', builtin.lsp_definitions, opts)
+      keymap('n', 'gr', builtin.lsp_references, opts)
+      keymap('n', 'gi', builtin.lsp_implementations, opts)
+      keymap('n', 'gT', builtin.lsp_type_definitions, opts)	end,
 	},
 }
